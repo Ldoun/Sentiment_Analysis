adb0caa2ddc36a0ec2439215a8f936f77ed594a8
Ldoun <ldoun8260@gmail.com>
2024-06-06 17:55:11 +0900

    feat: max_length

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ def args_for_model(parser, model):
     parser.add_argument('--num_classes', type=int, default=3, help='num_classes')
     parser.add_argument('--input_size', type=int, default=256, help='emb size')
     parser.add_argument('--hidden_size', type=int, default=256)
+    parser.add_argument('--max_length', type=int, default=512)
