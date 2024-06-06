b408a9a76abef576e02e7102c57b9547b4829660
Ldoun <ldoun8260@gmail.com>
2024-06-06 17:52:39 +0900

    fix: add tokenizer to RNN

--- models/model.py
+++ models/model.py
@@ -52,6 +52,7 @@ class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
         self.model = nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=2, batch_first=True, bidirectional=True)
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
         self.emb = nn.Embedding(num_embeddings=len(self.tokenizer), embedding_dim=input_size)
         self.linear = nn.Linear(self.model.config.hidden_size, num_classes)
         self.softmax = nn.Softmax()
